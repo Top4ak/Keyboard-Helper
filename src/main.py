290fd46b040e0af00eb30e4ddf2c57575cc74697
Top4ak <120311720+Top4ak@users.noreply.github.com>
2023-07-05 18:45:16 +0300

    Implementation of typing simulation and binds

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from pynput.keyboard import Controller
-from pynput import keyboard
+import pynput 
 import threading
 import time
 import sys
@@ -7,6 +7,7 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 from gui.ui_main import Ui_MainWindow
 import webbrowser
 from settings.binds import Settings
+import keyboard
 
 class AntiPasta(QMainWindow):
     def __init__(self):
@@ -29,13 +30,60 @@ class AntiPasta(QMainWindow):
         #Button connection
         self.ui.btn_info.clicked.connect(self.btn_info_clicked)
         self.ui.btn_settings.clicked.connect(self.btn_settings_clicked)
+        self.ui.btn_start.clicked.connect(self.start_simulation)
+        self.ui.btn_stop.clicked.connect(self.stop_simulation)
 
         self.ui.btn_save.clicked.connect(self.btn_save_clicked)
 
         self.ui.btn_github.clicked.connect(self.btn_github_clicked)
         self.ui.btn_telegram.clicked.connect(self.btn_telegram_clicked)
 
+        #Type bindings
+        self.is_typing = False
+
+        self.start_hk_is_binded = False
+        self.stop_hk_is_binded = False
+
+        if self.user_settings.get_start_hk() != '':
+            self.start_hk = keyboard.add_hotkey(self.user_settings.get_start_hk(), self.start_simulation)
+            self.start_hk_is_binded = True
+
+        if self.user_settings.get_stop_hk() != '':
+            self.stop_hk = keyboard.add_hotkey(self.user_settings.get_stop_hk(), self.stop_simulation)
+            self.stop_hk_is_binded = True
+    
+    def start_simulation(self):
+        self.is_typing = True
+        time.sleep(self.user_settings.get_starting_delay())
+
+        keyboard_controller = pynput.keyboard.Controller()
+        text = self.ui.textedit_userText.toPlainText()
+        
+        interval = self.user_settings.get_inteval()
+        random_factor = self.user_settings.get_random_factor()
+
+        if random_factor == 0:
+            for a in text:
+                if not self.is_typing:
+                    break
+                time.sleep(interval)
+                keyboard_controller.type(a)
+
+    def stop_simulation(self):
+        self.is_typing = False
+
     def btn_save_clicked(self):
+        if self.start_hk_is_binded:
+            keyboard.remove_hotkey(self.start_hk)
+            self.start_hk_is_binded = False
+
+        if self.stop_hk_is_binded:
+            keyboard.remove_hotkey(self.stop_hk)
+            self.stop_hk_is_binded = False
+
+        if self.ui.start_value.keySequence().toString() == self.ui.stop_value.keySequence().toString():
+            self.ui.stop_value.setKeySequence("")
+
         data = {
             "start_hot_key": self.ui.start_value.keySequence().toString(),
             "stop_hot_key": self.ui.stop_value.keySequence().toString(),
@@ -43,6 +91,14 @@ class AntiPasta(QMainWindow):
             "interval": self.ui.interval_value.value(),
             "random_factor": self.ui.randomFactor_value.value()
         }
+
+        if self.user_settings.get_start_hk() != '':
+            self.start_hk = keyboard.add_hotkey(self.user_settings.get_start_hk(), self.start_simulation)
+            self.start_hk_is_binded = True
+
+        if self.user_settings.get_stop_hk() != '':
+            self.stop_hk = keyboard.add_hotkey(self.user_settings.get_stop_hk(), self.stop_simulation)
+            self.stop_hk_is_binded = True
 
         self.user_settings.update(data)
 
